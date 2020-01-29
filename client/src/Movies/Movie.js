5796e13fe26852896438e0bbc353208d6bfeab51
Your Name <you@example.com>
2020-01-29 17:09:30 -0500

    mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from "react-router-dom"
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
+  let {id} = useParams();
  
   useEffect(() => {
-    const id = 1;
+
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -13,12 +15,13 @@ const Movie = (props) => {
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
+          // console.log(response.data)
         })
         .catch(error => {
           console.error(error);
         });
 
-  },[]);
+  },[id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
-);
+const SavedList = props => {
+  let history = useHistory();
+  let routeToHome = () => {
+    history.push("/")
+  }
+  
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <span className="saved-movie">{movie.title}</span>
+      ))}
+      <div onClick = {routeToHome} className="home-button">Home</div>
+    </div>
+  );
+};
 
 export default SavedList;
